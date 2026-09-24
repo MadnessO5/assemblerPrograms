3f4eb32572066c4d8ca54116c4d8a55ca536e5eb
Madness_O5 <ditabaskov@gmail.com>
2026-09-24 20:06:37 +0700

    Add files via upload

---
+++ boolean/ors/or.asm
@@ -0,0 +1,57 @@
+%include "stud_io.inc"
+
+global _start
+
+section .text
+
+_start:
+        call read_bin
+        mov ebx, eax
+
+        call read_bin
+        or eax, ebx
+
+        call print_bin
+        PUTCHAR 10
+
+        FINISH 0
+
+
+read_bin:
+        xor ecx, ecx
+
+.loop:
+        GETCHAR
+        cmp al, '0'
+        je .bit
+        cmp al, '1'
+        je .bit
+        jmp .done
+
+.bit:
+        shl ecx, 1
+        sub al, '0'
+        or cl, al
+        jmp .loop
+
+.done:
+        mov eax, ecx
+        ret
+
+
+print_bin:
+        mov ecx, 32
+
+.loop:
+        shl eax, 1
+        jc .one
+
+        PUTCHAR '0'
+        jmp .next
+
+.one:
+        PUTCHAR '1'
+
+.next:
+        loop .loop
+        ret
